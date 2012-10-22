81436db50253a63647bc362e951bc0c950eca909
Anton Ivanov <anton.al.ivanov@gmail.com>
2012-10-23 00:48:00 +0300

    Update src/currying/currying.js
    Removed a debugging call to "console.log" left in the code

--- src/currying/currying.js
+++ src/currying/currying.js
@@ -1,7 +1,6 @@
 (function(host) {
     if (!Function.prototype.curry) {
         Function.prototype.curry = function(argTransformer) {
-            console.log("argTransformer = ", argTransformer);
             var f = this;
             return function() {
                 var args = Array.prototype.slice.call(arguments, 0);
